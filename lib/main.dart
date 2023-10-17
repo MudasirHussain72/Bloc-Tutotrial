5b6e10bc7d1503302d68608460e1a87876eee11f
MudasirHussain72 <mudasirhussain41302@gmail.com>
2023-10-17 11:21:58 +0500

    adding bloc observer to observe all events/changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,61 @@
 import 'package:bloc_tutorial/bloc/products_bloc.dart';
 import 'package:bloc_tutorial/repo/products_repo.dart';
 import 'package:bloc_tutorial/screens/home_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(MyApp());
+  Bloc.observer = MyBlocObserver();
+  runApp(const MyApp());
+}
+
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    if (kDebugMode) {
+      print('onCreate -- ${bloc.runtimeType}');
+    }
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    if (kDebugMode) {
+      print('onEvent -- ${bloc.runtimeType}, $event');
+    }
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('onChange -- ${bloc.runtimeType}, $change');
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    if (kDebugMode) {
+      print('onTransition -- ${bloc.runtimeType}, $transition');
+    }
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    if (kDebugMode) {
+      print('onError -- ${bloc.runtimeType}, $error');
+    }
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    if (kDebugMode) {
+      print('onClose -- ${bloc.runtimeType}');
+    }
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -19,7 +69,7 @@ class MyApp extends StatelessWidget {
           create: (BuildContext context) => ProductsBloc(ProductsRepo()),
         ),
       ],
-      child: MaterialApp(
+      child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: HomeScreen(),
       ),
