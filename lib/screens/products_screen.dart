341f859cdcdd24263e7eb44fb6f940842c6c580a
MudasirHussain72 <mudasirhussain41302@gmail.com>
2023-10-17 12:22:53 +0500

    adding bloclistener to observe state & for toasts

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -20,56 +20,24 @@ class _ProductsScreenState extends State<ProductsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Products screen')),
-      // body: BlocBuilder<ProductsBloc, ProductsState>(
-      //   builder: (context, state) {
-      //     if (state is ProductsLoadingState) {
-      //       return const Center(child: CircularProgressIndicator.adaptive());
-      //     } else if (state is ProductsLoadedState) {
-      //       return ListView.builder(
-      //         itemCount: state.productsList.length,
-      //         itemBuilder: (context, index) {
-      //           var data = state.productsList[index];
-      //           return Text(data.title);
-      //         },
-      //       );
-      //     } else if (state is ProductsErrorState) {
-      //       return Center(child: Text('error state'));
-      //     } else {
-      //       return SizedBox();
-      //     }
-      //   },
-      // ),
-
-      body: BlocListener<ProductsBloc, ProductsState>(
-        listener: (context, state) {
-          if (state is ProductsLoadedState) {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                duration: Duration(seconds: 1), content: Text('data loaded')));
+      body: BlocBuilder<ProductsBloc, ProductsState>(
+        builder: (context, state) {
+          if (state is ProductsLoadingState) {
+            return const Center(child: CircularProgressIndicator.adaptive());
+          } else if (state is ProductsLoadedState) {
+            return ListView.builder(
+              itemCount: state.productsList.length,
+              itemBuilder: (context, index) {
+                var data = state.productsList[index];
+                return Text(data.title);
+              },
+            );
           } else if (state is ProductsErrorState) {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                duration: Duration(seconds: 1),
-                content: Text('data not loaded')));
+            return Center(child: Text('error state'));
+          } else {
+            return SizedBox();
           }
         },
-        child: BlocBuilder<ProductsBloc, ProductsState>(
-          builder: (context, state) {
-            if (state is ProductsLoadingState) {
-              return const Center(child: CircularProgressIndicator.adaptive());
-            } else if (state is ProductsLoadedState) {
-              return ListView.builder(
-                itemCount: state.productsList.length,
-                itemBuilder: (context, index) {
-                  var data = state.productsList[index];
-                  return Text(data.title);
-                },
-              );
-            } else if (state is ProductsErrorState) {
-              return Center(child: Text('error state'));
-            } else {
-              return SizedBox();
-            }
-          },
-        ),
       ),
     );
   }
