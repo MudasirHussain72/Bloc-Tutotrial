13a7b3c15e19fe19b6c6388fb01d6197218fbcab
MudasirHussain72 <mudasirhussain41302@gmail.com>
2023-10-17 12:06:26 +0500

    adding bloclistener to observe state & for toasts

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -20,24 +20,56 @@ class _ProductsScreenState extends State<ProductsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Products screen')),
-      body: BlocBuilder<ProductsBloc, ProductsState>(
-        builder: (context, state) {
-          if (state is ProductsLoadingState) {
-            return const Center(child: CircularProgressIndicator.adaptive());
-          } else if (state is ProductsLoadedState) {
-            return ListView.builder(
-              itemCount: state.productsList.length,
-              itemBuilder: (context, index) {
-                var data = state.productsList[index];
-                return Text(data.title);
-              },
-            );
+      // body: BlocBuilder<ProductsBloc, ProductsState>(
+      //   builder: (context, state) {
+      //     if (state is ProductsLoadingState) {
+      //       return const Center(child: CircularProgressIndicator.adaptive());
+      //     } else if (state is ProductsLoadedState) {
+      //       return ListView.builder(
+      //         itemCount: state.productsList.length,
+      //         itemBuilder: (context, index) {
+      //           var data = state.productsList[index];
+      //           return Text(data.title);
+      //         },
+      //       );
+      //     } else if (state is ProductsErrorState) {
+      //       return Center(child: Text('error state'));
+      //     } else {
+      //       return SizedBox();
+      //     }
+      //   },
+      // ),
+
+      body: BlocListener<ProductsBloc, ProductsState>(
+        listener: (context, state) {
+          if (state is ProductsLoadedState) {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                duration: Duration(seconds: 1), content: Text('data loaded')));
           } else if (state is ProductsErrorState) {
-            return Center(child: Text('error state'));
-          } else {
-            return SizedBox();
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                duration: Duration(seconds: 1),
+                content: Text('data not loaded')));
           }
         },
+        child: BlocBuilder<ProductsBloc, ProductsState>(
+          builder: (context, state) {
+            if (state is ProductsLoadingState) {
+              return const Center(child: CircularProgressIndicator.adaptive());
+            } else if (state is ProductsLoadedState) {
+              return ListView.builder(
+                itemCount: state.productsList.length,
+                itemBuilder: (context, index) {
+                  var data = state.productsList[index];
+                  return Text(data.title);
+                },
+              );
+            } else if (state is ProductsErrorState) {
+              return Center(child: Text('error state'));
+            } else {
+              return SizedBox();
+            }
+          },
+        ),
       ),
     );
   }
