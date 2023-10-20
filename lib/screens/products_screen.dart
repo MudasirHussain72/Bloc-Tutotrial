b53a0c078c19f9a4861dfc2702010a336324bcfb
MudasirHussain72 <mudasirhussain41302@gmail.com>
2023-10-20 11:05:49 +0500

    Adding StaggeredGrid view

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -1,6 +1,8 @@
 import 'package:bloc_tutorial/bloc/products_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class ProductsScreen extends StatefulWidget {
   const ProductsScreen({super.key});
@@ -72,17 +74,65 @@ class _ProductsScreenState extends State<ProductsScreen> {
           if (state is ProductsLoadingState) {
             return const Center(child: CircularProgressIndicator.adaptive());
           } else if (state is ProductsLoadedState) {
-            return ListView.builder(
-              itemCount: state.productsList.length,
-              itemBuilder: (context, index) {
-                var data = state.productsList[index];
-                return ListTile(title: Text(data.title));
-              },
+            return SingleChildScrollView(
+              child: Container(
+                margin: const EdgeInsets.all(12),
+                child: StaggeredGrid.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 4,
+                  crossAxisSpacing: 4,
+                  children: List.generate(state.productsList.length, (index) {
+                    if (index == 0) {
+                      // Display text before the first column
+                      return StaggeredGridTile.count(
+                        crossAxisCellCount: 1,
+                        mainAxisCellCount: 0.5,
+                        child: Align(
+                          alignment: Alignment.topLeft,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.9, // Adjust as needed
+                            child: Padding(
+                              padding: const EdgeInsets.all(0),
+                              child: Text(
+                                "${state.productsList.length} products found",
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    } else {
+                      // Display grid items for the second column
+                      return StaggeredGridTile.count(
+                        crossAxisCellCount: 1,
+                        mainAxisCellCount:
+                            index == 4 ? 3 : 1, // Adjusted mainAxisCellCount
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              color: Colors.transparent,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
+                          child: ClipRRect(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(15)),
+                            child: FadeInImage.memoryNetwork(
+                              placeholder: kTransparentImage,
+                              image: state.productsList[index - 1].image,
+                              // fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  }),
+                ),
+              ),
             );
           } else if (state is ProductsErrorState) {
-            return Center(child: Text('error state'));
+            return const Center(child: Text('error state'));
           } else {
-            return SizedBox();
+            return const SizedBox();
           }
         },
       ),
